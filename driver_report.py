e3da0357da93e67a6bfea13095bde4a3522e4e45
Kevin Clark <kevinwilliamclark@gmail.com>
2021-02-03 17:52:43 -0600

    adds Driver class to the dictionary and able to update miles and hours

--- driver_report.py
+++ driver_report.py
@@ -1,15 +1,24 @@
 import argparse
 import sys
 import datetime
+import operator
 from datetime import timedelta
 from collections import defaultdict
 
 
-d = defaultdict(list)
+d = defaultdict(dict)
+class Driver:
+    def __init__(self, name, miles=0, hours=0):
+        self.name = name
+        self.miles = miles
+        self.hours = hours
+    
+    def average(self):
+        return "%s: %s miles @ %s." % (self.name, self.miles, round(self.miles / self.hours))
+
 
 def initialize_driver(driver):
-    d[driver]
-    return d
+    d[driver] = Driver(driver)
 
 
 def driver_data(driver, start, stop, miles):
@@ -17,12 +26,13 @@ def driver_data(driver, start, stop, miles):
     y = datetime.datetime.strptime(stop, '%H:%M')
     td = timedelta(hours=y.hour, minutes=y.minute) - timedelta(hours=x.hour, minutes=x.minute)
     hours = td.seconds / 3600
-    print('hours', hours)
-    print('average speed', float(miles) / hours)
-    
+    mile = round(float(miles))
+    d[driver].miles += mile
+    d[driver].hours += hours
     
 
 def command_finder(filename):
+    """Open file and parse line commands"""
     with open(filename, 'r') as f:
         x = f.read().splitlines()
         for line in x:
@@ -31,31 +41,32 @@ def command_finder(filename):
                 driver = y[1]
                 initialize_driver(driver)
             elif y[0] == 'Trip':
-                print("here is the line",line)
                 driver = y[1]
                 start = y[2]
                 stop = y[3]
                 miles = y[4]
                 driver_data(driver, start, stop, miles)
         
-        
-
 
 def main(args):
+    """Parse terminal command"""
     parser = argparse.ArgumentParser(
         description="Receives driver data and creates report."
     )
-    print(args)
-
     if not args:
         parser.print_usage()
         sys.exit(1)
 
     filename = args[0]
     command_finder(filename)
-
-    print(d)
     
+    sorting = sorted(d, key=lambda name: d[name].miles, reverse=True)
+
+    for driver in sorting:
+        if d[driver].miles == 0:
+            print("%s: 0 miles" % d[driver].name)
+        else:
+            print(d[driver].average())
 
 if __name__ == "__main__":
     main(sys.argv[1:])
--- tests/test_driver_report.py
+++ tests/test_driver_report.py
@@ -3,7 +3,7 @@ import driver_report as dr
 
 
 filename = 'input.txt'
-driver_set = {'Dan': [], 'Lauren': [], 'Kumi': []}
+driver_set = {'Dan': {}, 'Lauren': {}, 'Kumi': {}}
 
 class TestDriver(unittest.TestCase):
 
@@ -27,18 +27,10 @@ class TestDriver(unittest.TestCase):
         names = ['Dan', 'Lauren', 'Kumi']
         for i in names:
             dr.initialize_driver(i)
-        self.assertEqual(dr.d, driver_set)
+        for index, driver in enumerate(dr.d):
+            self.assertEqual(names[index], dr.d[driver].name)
     
-
-    def test_initialize_driver_double(self):
-        """Checks for Driver initialization and doesn't create two drivers"""
-        names_double = ['Dan', 'Lauren', 'Kumi', 'Dan']
-        for i in names_double:
-            dr.initialize_driver(i)
-        print('Why is this like this', dr.d)
-        self.assertEqual(dr.d, driver_set)
-        
-    
+ 
     # def test_driver_data_times(self):
     #     """Checks the driver times."""
     #     driver = "Lauren"
